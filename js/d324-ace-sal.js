4ab192c977128a27781af3f0f754f7cceb15a618
Brian Nowell <briannowell@gmail.com>
2019-08-29 13:14:23 -0700

    Adding new appear animation paragraph behaviors and updating styles and templates

---
+++ js/d324-ace-sal.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * D324 Ace Scroll Appear Animations
+ */
+
+(function ($, Drupal) {
+
+  "use strict";
+
+  Drupal.behaviors.d324_ace_sal = {
+    attach: function (context, settings) {
+      sal();
+    }
+  };
+
+}(jQuery, Drupal));
